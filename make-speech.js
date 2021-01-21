8557cdb2005629c5c2e3089dc9984af0cd1a4acb
Lukas Benediktson <lub@frankly.dk>
2021-01-21 13:59:04 +0100

    krille

---
+++ make-speech.js
@@ -0,0 +1,33 @@
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech')
+const fs = require('fs')
+const util = require('util')
+
+const createAudio = (text) => {
+	// Creates a client
+	const client = new textToSpeech.TextToSpeechClient()
+	async function quickStart() {
+		// Construct the request
+		const request = {
+			input: { text: text },
+			// Select the language and SSML voice gender (optional)
+			voice: { languageCode: 'da-DK', ssmlGender: 'MALE' },
+			// select the type of audio encoding
+			audioConfig: { audioEncoding: 'MP3' },
+		}
+
+		// Performs the text-to-speech request
+		const [response] = await client.synthesizeSpeech(request)
+		// Write the binary audio content to a local file
+		const writeFile = util.promisify(fs.writeFile)
+		await writeFile(
+			`./users/welcome/output.mp3`,
+			response.audioContent,
+			'binary'
+		)
+		console.log('Audio content written to file: output.mp3')
+	}
+	quickStart()
+}
+
+createAudio('Er I klar til at tabe? Planetens største klunke, den mest woke mand på jorden. I ved det! Kristian vil spille lidt warzone')
